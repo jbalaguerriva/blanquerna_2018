86d10aa8112960989501467dedb1026e45ed0474
oscar marin <oscar.marin.ext@alten.es>
2019-02-08 15:46:15 +0100

    - SCRAPING

---
+++ scraping/README.md
@@ -0,0 +1,17 @@
+##Scraping de tablas
+
+####Fuente:
+
+En Google Spreadhseets, con =IMPORTHTML("https://en.wikipedia.org/wiki/List_of_countries_by_intentional_homicide_rate","table",4)
+
+https://en.wikipedia.org/wiki/List_of_countries_by_intentional_homicide_rate
+
+En Google Spreadsheets, con =IMPORTDATA("http://opendata-ajuntament.barcelona.cat/data/en/dataset/est-demo-canvi-domicili/resource/35c3e470-0d1f-4037-b647-b9bb280720f3")
+http://opendata-ajuntament.barcelona.cat/data/en/dataset/est-demo-canvi-domicili/resource/35c3e470-0d1f-4037-b647-b9bb280720f3
+
+
+####Plugins:
+
+https://chrome.google.com/webstore/detail/scraper/mbigbapnjcgaffohmbkdlecaccepngjd?hl=en
+
+https://chrome.google.com/webstore/detail/web-scraper/jnhgnonknehpejjnehehllkliplmbmhn?hl=en
---
+++ scraping/scrapeWikiTable.py
@@ -0,0 +1,69 @@
+# https://www.crummy.com/software/BeautifulSoup/bs4/doc/
+from bs4 import BeautifulSoup
+import requests
+import pprint
+import csv
+
+URL = "https://en.wikipedia.org/wiki/List_of_countries_by_intentional_homicide_rate"
+
+FILE_OUT = "homicides.csv"
+
+page = requests.get(URL)
+
+soup = BeautifulSoup(page.content, 'html.parser')
+
+
+# pprint.pprint(soup.findAll("a"))
+#
+# for node in soup.findAll("a"):
+#     pprint.pprint(node)
+#     pprint.pprint(node.attrs)
+#     pprint.pprint(node.get("href"))
+#     print("========================")
+#
+
+my_table = soup.select("table.wikitable")[2]
+# pprint.pprint(my_table)
+
+row_number = 0
+
+header = []
+data = []
+
+for row in my_table.select("tr"):
+    if row_number == 0:
+        for col in row.select("th"):
+            pprint.pprint(col.get_text().rstrip())
+            header.append(col.get_text().rstrip())
+    else:
+        if row_number > 1:
+            new_row = []
+
+            for col in row.select("td"):
+                pprint.pprint(col.get_text().lstrip().rstrip())
+                new_row.append(col.get_text().lstrip().rstrip())
+
+            data.append(new_row)
+    row_number+=1
+
+pprint.pprint(header)
+pprint.pprint(data)
+
+
+my_file = open(FILE_OUT, "w")
+
+csv_writer = csv.writer(my_file)
+
+csv_writer.writerow(header)
+
+for row in data:
+    csv_writer.writerow(row)
+
+my_file.close()
+# for table in soup.selectAll("table"):
+#     pprint.pprint(table)
+#
+# print("**********************")
+#
+# pprint.pprint(soup.selectAll("table")[1])
+
